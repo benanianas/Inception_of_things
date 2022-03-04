ca6e893f7ffcea5b4eef7b42bb6173dcef285023
Anas Benani <benanianas21@gmail.com>
2022-03-04 20:38:38 +0100

    Part2 Done

--- p2/setup.sh
+++ p2/setup.sh
@@ -1,4 +1,7 @@
-#!/bin/sh
-curl -sfL https://get.k3s.io | K3S_KUBECONFIG_MODE="644" sh -s - \
-kubectl create -f /vagrant/app-one.yaml \
-kubectl create -f /vagrant/service-one.yaml
+#!/bin/bash
+
+curl -sfL https://get.k3s.io | K3S_KUBECONFIG_MODE="644" INSTALL_K3S_EXEC="--flannel-iface eth1" sh -s -
+kubectl apply -f /vagrant/app-one.yaml >/dev/null 2>&1
+kubectl apply -f /vagrant/app-two.yaml >/dev/null 2>&1
+kubectl apply -f /vagrant/app-three.yaml >/dev/null 2>&1
+kubectl apply -f /vagrant/ingress.yaml >/dev/null 2>&1
